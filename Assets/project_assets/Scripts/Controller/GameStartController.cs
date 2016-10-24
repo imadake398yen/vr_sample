2622694d778f49c066ae2a746833672819736d98
falcon <imadake398yen@gmail.com>
2016-10-24 21:13:52 +0900

    SingletonMonoBehaviourのデモ

---
+++ Assets/project_assets/Scripts/Controller/GameStartController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStartController : MonoBehaviour {
+
+	private GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+		gameManager = GameManager.instance;
+		gameManager.score = 111;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/project_assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : SingletonMonoBehaviour<GameManager> {
+
+	private int Score;
+	public int score {
+		get { return Score + 1; }
+		set { Score = (value < 100) ? value : 100; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
